1aea049055d8276b4feec1470e75664c31de081a
scxfjiang <sc.xfjiang@gmail.com>
2022-07-03 13:51:03 +0000

    train yelp on GPU

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python main.py                                                          \
-    --dataset="/dataset/ml-100k"	    	                            \
+    --dataset="/dataset/yelp_dataset"	    	                        \
     --train_batch_size=1024                                             \
     --test_batch_size=1024                                              \
     --shuffle=True                                                      \
-    --num_epoch=500                                                     \
+    --num_epoch=100                                                     \
     --lr=1.0                                                            \
     --latent_dim=20                                                     \
     --use_cuda=True
